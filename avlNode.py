40a20d452e177aa3540d8e40563057b74b41d65c
Salvador Fuentes <salvador.fga@gmail.com>
2016-10-22 11:49:25 -0500

    First commit: add item, node and tree class
    item.py - Class that represents a directory Item
    avlNode.py - Class that represents an single Node
    avlTree.py - Class that represents an AVL tree with its methods
    
    Signed-off-by: Salvador Fuentes <salvador.fga@gmail.com>

---
+++ avlNode.py
@@ -0,0 +1,15 @@
+from item import DirectoryItem
+
+class avlnode(object):
+
+    def __init__(self, key, DirectoryItem):
+        self.left = None
+        self.right = None
+        self.key = key
+        self.DirectoryItem = DirectoryItem
+
+    def __str__(self):
+        return str(self.key)
+
+    def __repr__(self):
+        return str(self.key, self.DirectoryItem)
---
+++ item.py
@@ -0,0 +1,10 @@
+class DirectoryItem:
+    def __init__(self, name, last_name, address, mobile_phone, home_phone):
+        self.name = name
+        self.last_name = last_name
+        self.address = address
+        self.mobile_phone = mobile_phone
+        self.home_phone = home_phone
+
+    def __str__(self):
+        return str(self.name)
